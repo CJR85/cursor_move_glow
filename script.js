2adac351f37c259ddb67b47de284a1d17ea4d99e
Calvin Roberts <roberts.calvin85@gmail.com>
2023-09-19 20:54:49 +0100

    Add function to move and animate cursor

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+let div = document.querySelector('#cursor'),
+  body = document.querySelector('body');
+document.onmousemove = function (e) {
+  // Move cursor
+  cursor.style.top = e.pageY + 'px';
+  cursor.style.left = e.pageX + 'px';
+
+  // Animate background
+  body.style.backgroundPositionX = e.pageX / -4 + 'px';
+  body.style.backgroundPositionY = e.pageY / -4 + 'px';
+};
