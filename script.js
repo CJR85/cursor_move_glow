96fcf8c578ba0c72d624afde2530660f850379ee
Calvin Roberts <roberts.calvin85@gmail.com>
2023-09-19 21:53:10 +0100

    Add random text on numbers

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.onmousemove = function (e) {
       parseInt(text.style.top) - directionY * (Math.random() * 200) + 'px';
     text.style.opacity = 0;
     text.style.transform = 'scale(0.25)';
-    text.innerHTML = 'hi';
+    text.innerHTML = randomText();
 
     // Remove element
     setTimeout(function () {
@@ -38,3 +38,10 @@ document.onmousemove = function (e) {
     }, 1000);
   }, 10);
 };
+
+// Add random text on numbers
+function randomText() {
+  var text = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+  letter = text[Math.floor(Math.random() * text.length)];
+  return letter;
+}
