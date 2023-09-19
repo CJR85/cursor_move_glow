01e63a4572719a155c97d8b087460e1f4b9e56a7
Calvin Roberts <roberts.calvin85@gmail.com>
2023-09-19 21:15:25 +0100

    Add elements to body, move elememts across x & y axis

--- script.js
+++ script.js
@@ -9,4 +9,24 @@ document.onmousemove = function (e) {
   // Animate background
   body.style.backgroundPositionX = e.pageX / -4 + 'px';
   body.style.backgroundPositionY = e.pageY / -4 + 'px';
+
+  // Add elements to body
+  let element = createElement('div');
+  element.className = 'element';
+  body.prepend(element);
+
+  // Move elements to randomly across x & y axis
+  element.style.left = cursor.getBoundingClientRect().x + ' px';
+  element.style.top = cursor.getBoundingClientRect().y + ' px';
+
+  setTimeout(function () {
+    let text = document.querySelectorAll('.element')[0],
+      directionX = Math.random() < 0.5 ? -1 : 1,
+      directionY = Math.random() < 0.5 ? -1 : 1;
+
+    text.style.left =
+      parseInt(text.style.left) - directionX * (Math.random() * 200) + 'px';
+    text.style.top =
+      parseInt(text.style.top) - directionY * (Math.random() * 200) + 'px';
+  });
 };
